761c1777b743d38c451c06c073b98754bdd2f4d0
HannaAlek <140433583+HannaAlek@users.noreply.github.com>
2025-04-21 16:26:31 +0000

    Deploying to gh-pages from @ HannaAlek/goit-js-hw-10@16de82518b328f610844b2b77cf7189983631278 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/styles-DD3qoKza.js";import{f as o}from"./assets/vendor-EyZmBGcZ.js";const e=document.querySelector("#datetime-picker"),n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){t[0].getTime()}};console.log(e);o(e,n);
+//# sourceMappingURL=1-timer.js.map
