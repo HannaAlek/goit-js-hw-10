16de82518b328f610844b2b77cf7189983631278
HannaAlek <aniuta_alex@ukr.net>
2025-04-21 19:26:15 +0300

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,3 +1,21 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
+const inputData= document.querySelector('#datetime-picker')
+let userSelectedDate;
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        userSelectedDate=selectedDates[0].getTime();
+    },
+  };
+console.log(inputData)
+flatpickr(inputData, options)
+
+
+
+
+
