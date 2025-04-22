336ac60b056a7f84502fa01f68f6cf5e7b5ce9f2
HannaAlek <aniuta_alex@ukr.net>
2025-04-22 13:49:32 +0300

    update timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,6 +4,8 @@ import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
+const startBtn=document.querySelector('button[data-start]');
+startBtn.disabled=true;
 const inputData= document.querySelector('#datetime-picker')
 let userSelectedDate=null;
 let intervalId=null;
@@ -14,28 +16,17 @@ const options = {
     minuteIncrement: 1,
     onClose(selectedDates) {
         userSelectedDate=selectedDates[0].getTime();
-        if (userSelectedDate<=Date.now()){
-            iziToast.show({
-                        message:"Please choose a date in the future"
-                    });
-        }
-        else{
-            startBtn.disabled=false;
-            inputData.disabled=true;
-        }
-        console.log(convertMs(userSelectedDate))
-        // if (!userSelectedDate || isNaN(userSelectedDate)) return;
-        // const currentTime=Date.now();
-        // if(userSelectedDate<=currentTime){
-        //     iziToast.show({
-        //         message:"Please choose a date in the future"
-        //     });
-        //     return;
-        // }
-        // startBtn.disabled=false;
-        // inputData.disabled=true;
-        // updateTimer();
-        // intervalId=setInterval(updateTimer,1000);
+        const currentTime=Date.now();
+    if(userSelectedDate<=currentTime){
+        iziToast.show({
+            message:"Please choose a date in the future"
+        });
+        return;
+    }
+    startBtn.disabled=false;
+    inputData.disabled=false;
+    updateTimer();
+    intervalId=setInterval(updateTimer,1000);
     },
 };
 
@@ -43,7 +34,6 @@ const daysEl=document.querySelector('[data-days]');
 const hoursEl=document.querySelector('[data-hours]');
 const minsEl=document.querySelector('[data-minutes]');
 const seconsEl=document.querySelector('[data-seconds]');
-const startBtn=document.querySelector('button[data-start]');
 
  function updateTimer(){
     const currentTime = Date.now();
@@ -54,36 +44,42 @@ const startBtn=document.querySelector('button[data-start]');
         hoursEl.textContent='00';
         minsEl.textContent='00';
         seconsEl.textContent='00';
-        
-        // updateTimer();
-        intervalId=setInterval(updateTimer,1000);
         startBtn.disabled=true;
         inputData.disabled=true;
         return;
     }
-
     const{days, hours, minutes, seconds}=convertMs(deltaTime);
     daysEl.textContent=addLeadingZero(days);
     hoursEl.textContent=addLeadingZero(hours);
     minsEl.textContent=addLeadingZero(minutes);
     seconsEl.textContent=addLeadingZero(seconds);
+
+
 }
 
+
 function convertMs(ms) {
+    // Number of milliseconds per unit of time
     const second = 1000;
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
+  
+    // Remaining days
     const days = Math.floor(ms / day);
+    // Remaining hours
     const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
     const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
     const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-      return { days, hours, minutes, seconds };
+  
+    return { days, hours, minutes, seconds };
   }
   
-//   console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-//   console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-//   console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
   
 
 function addLeadingZero(value){
@@ -91,7 +87,7 @@ function addLeadingZero(value){
 }
 
  flatpickr(inputData,options)
- startBtn.addEventListener('click', updateTimer())
+ startBtn.addEventListener('click',updateTimer)
 
 
 
